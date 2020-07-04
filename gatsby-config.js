fc369adb93179958ab8b783f6f2e5b025a59c2b9
M.D <mroussel1980@gmail.com>
2020-07-04 08:42:47 +0200

    bug fix in gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,9 +32,9 @@ module.exports = {
           {src:"src/icons/icon-192x192.png",sizes:"192x192",type:"image/png"},
           {src:"src/icons/icon-256x256.png",sizes:"256x256",type:"image/png"},
           {src:"src/icons/icon-384x384.png",sizes:"384x384",type:"image/png"},
-          {src:"src/icons/icon-512x512.png",sizes:"512x512",type:"image/png"}]}
+          {src:"src/icons/icon-512x512.png",sizes:"512x512",type:"image/png"}
+        ]
       },
-      
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
